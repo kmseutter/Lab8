5f0c9f2dfee51d550cf959fe928c159d8f534220
Kayda Seutter <kmseutter@gmail.com>
2024-10-24 17:33:02 -0600

    added unit test for addCity function

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -11,6 +11,22 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class CustomListTest {
+    //private CustomList list;
 
+    public CustomList MockCityList(){
+        CustomList list = new CustomList(null, new ArrayList<>());
+        return list;
+    }
 
+    @Test
+    public void addCityTest(){
+        CustomList list = MockCityList();
+        int beforeAdd = list.getCount();
+        assertEquals(beforeAdd, list.getCount(), "Count should be 0");
+
+        City newCity = new City("Estevan", "SK");
+        list.add(newCity);
+        int afterAdd = beforeAdd+1;
+        assertEquals(list.getCount(), afterAdd, "City wasn't added");
+    }
 }
