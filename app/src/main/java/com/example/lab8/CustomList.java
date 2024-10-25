7c206528b8405eb70644ab6b4031006038469870
Kayda Seutter <kmseutter@gmail.com>
2024-10-24 18:16:40 -0600

    added countCities method and unit test

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -58,5 +58,8 @@ public class CustomList extends ArrayAdapter<City> {
     public void deleteCity(City city){
         cities.remove(city);
     }
+    public int countCities(){
+        //return cities.size();
+    }
 
 }
